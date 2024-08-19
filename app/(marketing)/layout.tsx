c4b4b9a367bbe2f9c34d70352f25d27d4f3bf360
NINE-J <rnwo47@gmail.com>
2024-08-19 14:04:33 +0900

    [M] Marketing page
    - footer, navbar

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,17 +1,14 @@
-const MarketingLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
+import { Footer } from "./_components/footer";
+import { Navbar } from "./_components/navbar";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
-      {/* TODO: Navbar */}
-      <main className="pt-40 pb-20 bg-slate-100">
-        {children}
-      </main>
-      {/* TODO: Footer */}
+      <Navbar />
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
     </div>
   );
-}
- 
+};
+
 export default MarketingLayout;
